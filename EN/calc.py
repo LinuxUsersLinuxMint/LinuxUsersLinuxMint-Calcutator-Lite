132c7e04e5c7a56cb670ea71c73e32185d46f4da
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-10-08 14:53:43 +0300

    Updated Calc-Lite 0.2.8

--- EN/calc.py
+++ EN/calc.py
@@ -5,23 +5,22 @@
 # Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint
 # A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint
 
-cmd=str(input('calc> '))
+cmd=str("calc> ")
 
-if cmd=="calc":
-   print("calc> Transactions you can enter: ")
-   print("Addition\nSubraction\n\Multiplication\nDivision\nPercentage\n 1,2,3,4")
-   n1=float(input('{0} Enter The 1st Number: '. format(cmd)))
-   n2=float(input('{0} Enter The 2st Number: '. format(cmd)))
-   process=input('{0} Enter the Transaction You Want to Perform: '. format(cmd))
-   if process=="1": 
-      print("{0} + {1} = {2}". format(n1,n2,n1+n2))  
-   elif process=="2":
-       print("{0} - {1} = {2}". format(n1,n2,n1-n2))
-   elif process=="3":
-       print("{0} * {1} = {2}". format(n1,n2,n1*n2))
-   elif process=="4":
-       print("{0} / {1} = {2}". format(n1,n2,n1/n2))
-   elif process=="5":
-       print("{0} % {1} = {2}". format(n1,n2,n1%n2))
-   else:
-       print("Invalid Process!")
+print("calc> Transactions you can enter: ")
+print("Addition\nSubraction\n\Multiplication\nDivision\nPercentage\n 1,2,3,4")
+n1=float(input('{0} Enter The 1st Number: '. format(cmd)))
+n2=float(input('{0} Enter The 2st Number: '. format(cmd)))
+process=input('{0} Enter the Transaction You Want to Perform: '. format(cmd))
+if process=="1": 
+    print("{0} + {1} = {2}". format(n1,n2,n1+n2))  
+elif process=="2":
+    print("{0} - {1} = {2}". format(n1,n2,n1-n2))
+elif process=="3":
+    print("{0} * {1} = {2}". format(n1,n2,n1*n2))
+elif process=="4":
+    print("{0} / {1} = {2}". format(n1,n2,n1/n2))
+elif process=="5":
+    print("{0} % {1} = {2}". format(n1,n2,n1%n2))
+else:
+    print("Invalid Process!")
--- TR/calc.py
+++ TR/calc.py
@@ -5,23 +5,22 @@
 # Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint
 # A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint
 
-cmd=str(input('calc> '))
+cmd=str("calc> ")
 
-if cmd=="calc":
-   print("calc> Girebileceğiniz işlemler: ")
-   print("top\ncık\n\carp\nbol\nyuzde\n 1,2,3,4")
-   s1=float(input('{0} 1. sayiyi giriniz: '. format(cmd)))
-   s2=float(input('{0} 2. sayiyi giriniz: '. format(cmd)))
-   islem=input('{0} Gerçekleştirmek İstediğiniz İşlemi Giriniz: '. format(cmd))
-   if islem=="1": 
-      print("{0} + {1} = {2}". format(s1,s2,s1+s2))  
-   elif islem=="2":
-       print("{0} - {1} = {2}". format(s1,s2,s1-s2))
-   elif islem=="3":
-       print("{0} * {1} = {2}". format(s1,s2,s1*s2))
-   elif islem=="4":
-       print("{0} / {1} = {2}". format(s1,s2,s1/s2))
-   elif islem=="5":
-       print("{0} % {1} = {2}". format(s1,s2,s1%s2))
-   else:
-       print("Geçersiz İşlem")
+print("calc> Girebileceğiniz işlemler: ")
+print("top\ncık\n\carp\nbol\nyuzde\n 1,2,3,4")
+s1=float(input('{0} 1. sayiyi giriniz: '. format(cmd)))
+s2=float(input('{0} 2. sayiyi giriniz: '. format(cmd)))
+islem=input('{0} Gerçekleştirmek İstediğiniz İşlemi Giriniz: '. format(cmd))
+if islem=="1": 
+    print("{0} + {1} = {2}". format(s1,s2,s1+s2))  
+elif islem=="2":
+    print("{0} - {1} = {2}". format(s1,s2,s1-s2))
+elif islem=="3":
+    print("{0} * {1} = {2}". format(s1,s2,s1*s2))
+elif islem=="4":
+    print("{0} / {1} = {2}". format(s1,s2,s1/s2))
+elif islem=="5":
+    print("{0} % {1} = {2}". format(s1,s2,s1%s2))
+else:
+    print("Geçersiz İşlem")
